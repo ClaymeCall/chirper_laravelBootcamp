8624c5ef70c4ead325389c0f5e772aa3d08a6dc6
Clement Callejon <clement.callejon@tutanota.com>
2026-01-06 17:42:41 +0100

    Formatted Blade code with freshly fixed Blade Treesitter parsing

--- chirper/resources/views/home.blade.php
+++ chirper/resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="max-w-2xl mx-auto">
         <h1 class="text-3xl font-bold mt-8">Latest Chirps</h1>
 
-    <x-chirpForm/>
+        <x-chirpForm/>
 
-    <x-chirpFeed :chirps="$chirps" />
+        <x-chirpFeed :chirps="$chirps" />
 </x-layout>
